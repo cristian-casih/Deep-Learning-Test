b8a3f91a33be145235b084d6b1743de92ad3f92c
cristian-casih <cristiancasih1@gmail.com>
2018-12-04 12:38:47 -0300

    martes 13:45

--- src/test_project/settings.py
+++ src/test_project/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'registration',
     'boletin'
 ]
 
@@ -137,3 +139,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_env", "static_root")
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_env", "media_root")
+
+
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/'
+SITE_ID = 1
